0a502d287a8b322a7a5c9a34c37618e0d7d73f40
Alejandro <diaza913@gmail.com>
2018-02-13 22:12:39 -0600

    Configuración incial del servidor

--- src/DataBase/index.js
+++ src/DataBase/index.js
@@ -1,1 +1,13 @@
-console.log("Hello node.js");
+var port = process.env.PORT || 3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req , res) {
+
+    res.send("Hello WOrld")
+
+});
+
+
+app.listen(port);
