925f0b5b518dd6c6072963505349ddb9779b4e51
Alejandro <diaza913@gmail.com>
2018-02-13 23:00:22 -0600

    Editado, archivo principal de node.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+var express = require('express')
+var app = express();
 var port = process.env.PORT || 3000
 
-const express = require('express');
-const app = express();
 
-app.get('/', function (req , res) {
-
-    res.send("Hello WOrld")
-
+app.get('*', function (req, res) {
+    res.send("Hello world" , 200);
 });
 
 
